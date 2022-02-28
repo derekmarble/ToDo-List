fcfb956337c9b1a82d31e640924c7c57ba660286
Derek Marble <dmarble20@gmail.com>
2022-02-27 21:22:03 -0500

    5.17: Finished To Do List App with foreground notifications, final polish

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 private let dateFormatter: DateFormatter = {
     print("I just created a date formatter!")
@@ -35,6 +36,10 @@ class ToDoDetailTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // setup foreground notification
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appActiveNotification), name: UIApplication.didBecomeActiveNotification, object: nil)
+        
         //hide the keyboard if we tap out of field or view
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
         tap.cancelsTouchesInView = false
@@ -50,6 +55,11 @@ class ToDoDetailTableViewController: UITableViewController {
  
         
         
+    }
+    
+    @objc func appActiveNotification() {
+        print("The app just came to the foreground")
+        updateReminderSwitch()
     }
     
     func updateUserInterface() {
